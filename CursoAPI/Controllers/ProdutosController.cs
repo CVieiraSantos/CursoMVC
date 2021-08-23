eff67f7abd27fed2e1cb3f924f79dca1b2224076
CVieiraSantos <cadu.vieira2276@gmail.com>
2021-08-22 22:22:33 -0300

    Criacao da controller ProdutosController dentro do projeto CursoAPI

---
+++ CursoAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,121 @@
+﻿using CursoMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public ProdutosController(Context context)
+        {
+            _context = context;
+        }
+
+        //GET: api/Produtos
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        //GET: api/Produtos/MVC
+        [HttpGet(template: "{id}")]
+
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+
+        //PUT: api/Categorias/MVC
+        [HttpPut(template: "{id}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+
+                else
+                {
+                    throw;
+                }
+
+            }
+
+            return NoContent();
+
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //POST: api/Produtos/MVC
+        [HttpPost]
+
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", routeValues: new { id = produto.Id }, value: produto);
+        }
+
+        //DELETE: api/Produto/MVC   
+        [HttpDelete]
+
+        public async Task<ActionResult<Produto>> DeleteProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return produto;
+        }
+
+        private bool ProdutosExists(int id)
+        {
+            return _context.Produtos.Any(e => e.Id == id);
+        }
+
+
+    }
+}
